44b416dc3f2c92490ea5b6d79363b3c525336773
nomad <asp.anthony1@gmail.com>
2019-11-29 13:22:22 -0800

    separated system db handling and individual db handling
    +MySqlDataBase for individual database handleing
    +Sysytem_Sql_Data_Base_Handler system wide data base handling

--- app/db_handler.py
+++ app/db_handler.py
@@ -2,8 +2,8 @@ import mysql.connector as msc
 from app.log_handler import get_logger
 
 
-class MySqlDataBase:
 
+class System_Sql_Data_Base_Handler():
     def __init__(self):
         self.logger = get_logger()
         self.db = self.connect_to_db()
@@ -16,7 +16,16 @@ class MySqlDataBase:
 
 
     def create_db(self, name):
-        self.myc.execute('CREATE DATABASE {0}'.format(name))
+        try:
+            self.myc.execute('CREATE DATABASE {0}'.format(name))
+        except msc.errors.DatabaseError as e:
+            print(f"The {name} data base already exists")
+            self.logger.error(e)
+        else:
+            msg = "data base {0} was created".format(name)
+            print(msg)
+            self.logger.info(msg=msg)
+
 
     def get_all_dbs(self):
         self.myc.execute('SHOW DATABASES')
@@ -26,3 +35,53 @@ class MySqlDataBase:
         self.myc.execute('SHOW DATABASES')
         for i in self.myc:
             print(i)
+
+    def del_db(self, name):
+        self.myc.execute("DROP DATABASE {0}".format(name))
+
+
+
+class MySqlDataBase:
+
+    def __init__(self, name):
+        self.logger = get_logger()
+        self.name = name
+        self.db = self.connect_to_db(name)
+        self.myc = self.db.cursor()
+
+    def connect_to_db(self, name):
+        mydb = msc.connect(host='localhost',
+                           user='root',
+                           passwd='password',
+                           auth_plugin='mysql_native_password',
+                           database=name)
+        self.logger.info(msg="Connect to Database: {0}".format(name))
+        return mydb
+
+
+    def create_table(self, name, primary_key, pk_data_type):
+        # try:
+        self.myc.execute(f'CREATE Table {name} ({primary_key} {pk_data_type});')
+        # except msc.errors.DatabaseError as e:
+        #     print(f"The {name} data base already exists")
+        #     self.logger.error(e)
+        # else:
+        #     msg = "data base {0} was created".format(name)
+        #     print(msg)
+        #     self.logger.info(msg=msg)
+
+
+    def del_table(self, name):
+        self.myc.execute("DROP {0}".format(name))
+
+
+    def show_all_tables(self):
+        self.myc.execute("SHOW TABLES ")
+        print(*self.myc)
+
+
+    def show_table(self,name):
+        self.myc.execute("SHOW TABLES ")
+
+
+
--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from app.log_handler import get_logger
-from app.db_handler import MySqlDataBase
+from app.db_handler import MySqlDataBase, System_Sql_Data_Base_Handler
 
 logger = get_logger()
 
 logger.info(msg='tesgdasdfo')
 
+#
+# sys_db_handler = System_Sql_Data_Base_Handler()
+# sys_db_handler.create_db('giggffe')
+# print(*sys_db_handler.get_all_dbs())
+# sys_db_handler.del_db('giffe')
+#
+# print(*sys_db_handler.get_all_dbs())
+#
 
-mydb = MySqlDataBase()
+
+mdb = MySqlDataBase("giraffe")
+print(mdb.show_all_tables())
+
